505bf6c75cfba5b8763e22fe8266466fe58e9b4e
guang <1412118291@qq.com>
2024-11-19 20:13:15 +0800

    添加删除评论功能，添加不显示密码功能

--- src/main/java/dashstar/model/User.java
+++ src/main/java/dashstar/model/User.java
@@ -1,5 +1,6 @@
 package dashstar.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -20,6 +21,7 @@ public class User implements Serializable {
     @Column(name = "nickname")
     private String nickname;
 
+    @JsonIgnore   //不显示password
     @Column(name = "password")
     private String password;
 
--- src/main/java/dashstar/util/BCryptUtil.java
+++ src/main/java/dashstar/util/BCryptUtil.java
@@ -2,9 +2,11 @@ package dashstar.util;
 import org.mindrot.jbcrypt.BCrypt;
 public class BCryptUtil {
     public static String hashPassword(String plainPassword) {
-        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+        return plainPassword;
+//        拓展:return null;
     }
     public static boolean checkPassword(String plainPassword, String hashedPassword) {
         return BCrypt.checkpw(plainPassword, hashedPassword);
+//        拓展:return true;
     }
 }
